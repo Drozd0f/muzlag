bafa3531635f2248f362789a8ff731e51477e445
Drozd0f <600dan600@gmail.com>
2022-07-20 13:11:42 +0300

    Implementation play music in channel

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # muzlag
 [![Lint](https://github.com/Drozd0f/muzlag/actions/workflows/linter.yml/badge.svg)](https://github.com/Drozd0f/muzlag/actions/workflows/linter.yml)
+
+sudo apt install ffmpeg
--- main.py
+++ main.py
@@ -1,23 +1,71 @@
 import os
-import time
+import asyncio
 
-import discord
+from discord import VoiceChannel, VoiceClient, FFmpegPCMAudio, PCMVolumeTransformer
 from discord.ext import commands
+
+from src.youtube import YTDLSource
 
 
 bot = commands.Bot(command_prefix='>>')
 
 
-@bot.command()
-async def ping(ctx: commands.context.Context) -> discord.Message:
+@bot.command(name='ping', help='Healthcheck')
+async def ping(ctx: commands.context.Context):
     await ctx.send('pong')
 
 
 @bot.command()
-async def play(ctx: commands.context.Context):
-    await ctx.author.voice.channel.connect()
-    time.sleep(5)
+async def danillo(ctx: commands.context.Context):
+    if not ctx.message.author.voice:
+        await ctx.send(f"{ctx.message.author.name} is not connected to a voice channel")
+        return
+    channel: VoiceChannel = ctx.message.author.voice.channel
+    try:
+        await channel.connect()
+        player = PCMVolumeTransformer(FFmpegPCMAudio('./meme/danillo.mp4'))
+        ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+    except commands.errors.ClientException:
+        pass
+
+    while ctx.voice_client.is_playing():
+        await asyncio.sleep(1)
+
+    ctx.voice_client.stop()
     await ctx.voice_client.disconnect()
+
+
+@bot.command()
+async def play(ctx: commands.context.Context, url: str):
+    if not ctx.message.author.voice:
+        await ctx.send(f"{ctx.message.author.name} is not connected to a voice channel")
+        return
+    channel: VoiceChannel = ctx.message.author.voice.channel
+    try:
+        await channel.connect()
+        server = ctx.message.guild
+        voice_channel = server.voice_client
+        async with ctx.typing():
+            filename = await YTDLSource.from_url(url, loop=bot.loop)
+            voice_channel.play(FFmpegPCMAudio(filename[0]))
+        await ctx.send(f'Now playing: **{filename[1]}**')
+    except commands.errors.ClientException:
+        pass
+
+    while ctx.voice_client.is_playing():
+        await asyncio.sleep(1)
+
+    ctx.voice_client.stop()
+    await ctx.voice_client.disconnect()
+
+
+@bot.command()
+async def stop(ctx: commands.context.Context):
+    voice_client: VoiceClient = ctx.message.guild.voice_client
+    if voice_client.is_connected():
+        await voice_client.disconnect()
+    else:
+        await ctx.send("The bot is not connected to a voice channel.")
 
 
 def main():
