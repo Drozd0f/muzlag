62f5f63ce145fc0a3ae17c3aa7f7ba1d74452c90
Drozd0f <600dan600@gmail.com>
2022-10-03 09:57:12 +0300

    Create index for table playlists column name

--- bot/database/migrations/001_init.down.sql
+++ bot/database/migrations/001_init.down.sql
@@ -2,4 +2,5 @@ DROP TABLE IF EXISTS songs;
 DROP TABLE IF EXISTS playlists;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS songs_playlists;
+DROP INDEX IF EXISTS playlists_name;
 DROP INDEX IF EXISTS songs_playlists_idx;
